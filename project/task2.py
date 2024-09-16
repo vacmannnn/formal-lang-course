f6ca69dfe442fbd78b35fb6afdb1dfe91c171fef
Alexander <opera9876@rambler.ru>
2024-09-16 21:48:21 +0300

    task2: minimize dfa

--- project/task2.py
+++ project/task2.py
@@ -9,7 +9,7 @@ from pyformlang.regular_expression import Regex
 
 
 def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
-    Regex(regex).to_epsilon_nfa().to_deterministic()
+    return Regex(regex).to_epsilon_nfa().to_deterministic().minimize()
 
 
 def graph_to_nfa(
